4fe7686fbce01b496bf855a31195991758db096d
Kaliyaeden <kaliyaeden@gmail.com>
2016-06-15 07:49:29 +0800

    调整界面。

--- WebContent/assets/admin/js/show.js
+++ WebContent/assets/admin/js/show.js
@@ -1,16 +1,45 @@
 $(document).ready(function(){
 
+    //Set tabs
+    $('#tabsMenu').tabs({
+        onSelect:function(title,index){
+            if(index == 0){
+                showDatagrid('#check');
+            }else if(index == 1){
+                showDatagrid('#push');
+            }else if(index ==2){
+                showDatagrid('#pushed');
+            }
+        }
+    });
 });
+
+function showDatagrid(tabsId){
+    $(tabsId).datagrid({
+        url:'datagrid_data.json',
+        columns:[[
+            {field:'code',title:'Code',width:100},
+            {field:'name',title:'Name',width:100},
+            {field:'price',title:'Price',width:100,align:'right'}
+        ]]
+    });
+}
+
+
 
 var url;
 
-function check(){
-    var row = $('#wait_check').datagrid('getSelected');
+function normalCheck(){
+    var row = $('#check').datagrid('getSelected');
     if (row){
-        $('#dlg').dialog('open').dialog('center').dialog('setTitle','Edit User');
+        $('#check-dlg').dialog('open').dialog('center').dialog('setTitle','待审核');
         $('#fm').form('load',row);
         url = 'update_user.php?id='+row.id;
     }
+}
+
+function multipleCheck(){
+
 }
 
 function saveUser(){
@@ -28,7 +57,7 @@ function saveUser(){
                 });
             } else {
                 $('#dlg').dialog('close');        // close the dialog
-                $('#wait_check').datagrid('reload');    // reload the user data
+                $('#check').datagrid('reload');    // reload the user data
             }
         }
     });
