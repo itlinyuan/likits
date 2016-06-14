c1c1311392060c6300b3e6aa56935f7db424cee3
Kaliyaeden <kaliyaeden@gmail.com>
2016-06-14 21:02:11 +0800

    Add Show position materials.
    Signed-off-by: Kaliyaeden <kaliyaeden@gmail.com>

---
+++ WebContent/assets/admin/js/news.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+});
+
+var url;
+
+function check(){
+    var row = $('#waitcheck').datagrid('getSelected');
+    if (row){
+        $('#dlg').dialog('open').dialog('center').dialog('setTitle','Edit User');
+        $('#fm').form('load',row);
+        url = 'update_user.php?id='+row.id;
+    }
+}
+
+function saveUser(){
+    $('#fm').form('submit',{
+        url: url,
+        onSubmit: function(){
+            return $(this).form('validate');
+        },
+        success: function(result){
+            var result = eval('('+result+')');
+            if (result.errorMsg){
+                $.messager.show({
+                    title: 'Error',
+                    msg: result.errorMsg
+                });
+            } else {
+                $('#dlg').dialog('close');        // close the dialog
+                $('#waitcheck').datagrid('reload');    // reload the user data
+            }
+        }
+    });
+}
